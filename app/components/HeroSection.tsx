4f6bb8cac8edf385406cd7dd992567c30228d3b7
MalaikaSaleem111 <161643745+MalaikaSaleem111@users.noreply.github.com>
2024-11-17 19:15:08 +0500

    Update HeroSection.tsx

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection(){
         <div className="flex flex-col md:flex-row items-center bg-slate-100  ">
            <div>
             <p className="w-1/2 md:block hidden text-center md:text-left text-5xl px-14 font-extrabold">Pick your <br/>perfect book!</p>
-            <button className="bg-slate-400 w-28 text-center text-xl font-bold rounded-lg mx-14 my-4 px-4 h-14">Explore</button>
+            <button className="bg-slate-400 w-28 md:block hidden text-center text-xl font-bold rounded-lg mx-14 my-4 px-4 h-14">Explore</button>
             </div>
             
             <Image src="/book.jpg" alt="poster" height={500} width={500} className="w-full md:w-1/2 rounded-md"/>
