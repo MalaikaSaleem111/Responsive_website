2e0df9870c56d928545590fcb85f44f3708acf2b
MalaikaSaleem111 <161643745+MalaikaSaleem111@users.noreply.github.com>
2024-11-17 18:41:41 +0500

    Update Featuring.tsx

--- app/components/Featuring.tsx
+++ app/components/Featuring.tsx
@@ -5,13 +5,13 @@ export default function Featuring(){
           <h2 className="text-2xl font-extrabold mt-6 px-5 text-center">Shop By Category</h2>
           <div className="w-full flex flex-wrap justify-evenly px-4 my-6 gap-y-6 border-2 border-gray-200 p-4">
               <div className="w-1/2 lg:w-1/4 text-center">
-              <Image src="/self-help.jpg" alt="self-help" width={500} height={500} className="w-auto mx-auto" />
+              <Image src="/self-help.jpg" alt="self-help" width={500} height={500} className="w-36 h-60 mx-auto" />
               <p className="text-center font-bold text-lg mb-2">Self Help</p>
               <button className="w-28 h-14 font-bold p-4  text-white bg-slate-600 rounded-full">See More</button>
               </div>
 
               <div className="w-1/2 lg:w-1/4 text-center">
-              <Image src="/Philosophy.jpg" alt="Philosophy" width={500} height={500} className="w-auto mx-auto" />
+              <Image src="/Philosophy.jpg" alt="Philosophy" width={500} height={500} className="w-36 h-60 mx-auto" />
               <p className="text-center font-bold text-lg mb-2">Philosophy</p>
               <button className="w-28 h-14 font-bold p-4 text-white bg-slate-600 rounded-full">See More</button>
               </div>
@@ -40,7 +40,7 @@ export default function Featuring(){
               </div>
 
               <div className="w-1/2 lg:w-1/4 text-center">
-              <Image src="/new2.jpg" alt="book2" width={500} height={500} className="w-auto mx-auto" />
+              <Image src="/new2.jpg" alt="book2" width={500} height={500} className="w-36 h-60 mx-auto" />
               <p className="text-center font-semibold text-base mb-2">Unlearning Silence <br/>
               By: Elaine Lin Hering</p>
               <p className="text-red-600">Rs 2,996.00</p>
@@ -49,7 +49,7 @@ export default function Featuring(){
           
             
               <div className="w-1/2 lg:w-1/4 text-center">
-              <Image src="/new3.jpg" alt="book3" width={500} height={500} className="w-auto mx-auto" />
+              <Image src="/new3.jpg" alt="book3" width={500} height={500} className="w-36 h-60 mx-auto" />
               <p className="text-center font-semibold text-base mb-2">7 Rules of Power<br/>
               By: Jeffrey Pfeffer</p>
               <p className="text-red-600">Rs 1,636.00</p>
@@ -57,7 +57,7 @@ export default function Featuring(){
               </div>
 
               <div className="w-1/2 lg:w-1/4 text-center">
-              <Image src="/new4.jpg" alt="book4" width={500} height={500} className="w-auto mx-auto" />
+              <Image src="/new4.jpg" alt="book4" width={500} height={500} className="w-36 h-60 mx-auto" />
               <p className="text-center font-semibold text-base mb-2">Forgiving What You Can't Forget<br/>
               By: Lysa TerKeurst</p>
               <p className="text-red-600">Rs 1,476.00</p>
